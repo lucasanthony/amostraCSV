7215390b3007dc619f671950676eeda10bc35baa
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2018-12-14 10:45:24 -0300

    atualiozado

--- teste.py
+++ teste.py
@@ -1,16 +1,31 @@
 #coding: utf-8
 
+def csvtolist(arquivo,separador):
+	import csv
+	lista = []
+	arquivo = '%s.csv' %arquivo
+	with open(arquivo) as csvfile:
+   		dados = csv.reader(csvfile, delimiter=separador)
+   		for linha in dados:
+   			lista.append(linha)
+	return lista
+
+
+def randomLines(lista) :
+	retorno = [];
+
+
 def randomLines():
 	import random
 	saida = []
 	fid = open("dados.csv", "r")
 	dados = fid.readlines()
 	fid.close()
-	dados.pop(0)
-
+	saida = []
+	saida.append(dados[0])
 	random.shuffle(dados)
 
-	for i in range(50000):
+	for i in range(10000):
 		saida.append(dados[i])
 	
 	fid = open("amostra.csv", "w")
