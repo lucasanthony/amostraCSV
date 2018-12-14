3d2837d219fd3bd285b2a8d377bf7af7c7ace139
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2018-12-14 09:34:42 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,20 +1,5 @@
 #coding: utf-8
 
-def csvtolist(arquivo,separador):
-	import csv
-	lista = []
-	arquivo = '%s.csv' %arquivo
-	with open(arquivo) as csvfile:
-   		dados = csv.reader(csvfile, delimiter=separador)
-   		for linha in dados:
-   			lista.append(linha)
-	return lista
-
-
-def randomLines(lista) :
-	retorno = [];
-
-
 def randomLines():
 	import random
 	saida = []
